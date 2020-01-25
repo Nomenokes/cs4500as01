33a166627b76bc042286be3577d2fccbd7b6b70c
Bryce Costawong <44653582+Nomenokes@users.noreply.github.com>
2020-01-24 19:47:01 -0500

    updated string.h, object.h with comments

--- object.h
+++ object.h
@@ -2,6 +2,11 @@
 
 class Object {
     public:
+    
+    // returns if this object equals the other object
+    // other can be NULL, in which case return false
     bool equals (Object* other);
+    
+    // Hashes this object. Must be the same if equal.
     size_t hash_code();
 };
--- string.h
+++ string.h
@@ -4,7 +4,16 @@
 
 class String : public Object {
     public:
+    
+    // returns the size of this string
     size_t size();
+    
+    // Returns a char buffer containing the characters of this string
+    // Caller should not modify return, as if it is const
     char* to_char_array();
+    
+    // Compares this string to another string,
+    // returns <0 if this is less than the other,
+    //    =0 if they are equal, and >0 if this is greater.
     int compare_to(String* other);
 };
