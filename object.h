f2db931f82ba83f4b67899abc53376db412754d5
Bryce Costawong <44653582+Nomenokes@users.noreply.github.com>
2020-01-28 21:22:18 -0500

    typo in previous

--- object.h
+++ object.h
@@ -5,7 +5,7 @@ class Object {
     
     // returns if this object equals the other object
     // other can be NULL, in which case return false
-    virtual bool equals (Object* other);
+    virtual bool equals (const Object* other);
     
     // Hashes this object. Must be the same if equal.
     virtual size_t hash();
