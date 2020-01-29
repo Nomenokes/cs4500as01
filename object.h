15c23e7aae12beb0b3943871cb7efe7a0dd78206
Bryce Costawong <44653582+Nomenokes@users.noreply.github.com>
2020-01-28 21:11:43 -0500

    updated to mesh with class specs

--- object.h
+++ object.h
@@ -5,8 +5,8 @@ class Object {
     
     // returns if this object equals the other object
     // other can be NULL, in which case return false
-    bool equals (Object* other);
+    virtual bool equals (Object* other);
     
     // Hashes this object. Must be the same if equal.
-    size_t hash_code();
+    virtual size_t hash();
 };
